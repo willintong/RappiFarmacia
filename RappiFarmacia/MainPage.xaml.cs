ffa1bef748a9be5b8707cc43d8e6cb07dec96ad1
Willintong Andrés López Zúñiga <willintong.lopez@sofka.com.co>
2019-09-09 09:58:42 -0500

    login

---
+++ RappiFarmacia/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using RappiFarmacia.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RappiFarmacia
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Home_Click(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new HomePage());
+            IsPresented = false;
+        }
+
+        private void Domicilio_Click(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new DomicilioPage());
+            IsPresented = false;
+        }
+
+        private void Pedidos_Click(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new PedidosPage());
+            IsPresented = false;
+        }
+    }
+}
---
+++ RappiFarmacia/Views/LoginPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RappiFarmacia.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+        private async void Ingresar_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(TextUsuario.Text))
+            {
+                await DisplayAlert("Validación", "El nombre de usuario es requerido", "OK");
+                TextUsuario.Focus();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(TextPassword.Text))
+            {
+                await DisplayAlert("Validación", "La contraseña es requerida", "OK");
+                TextPassword.Focus();
+                return;
+            }
+
+            await Navigation.PushAsync(new HomePage());
+        }
+    }
+}
